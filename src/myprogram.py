ea3e56fb87d8605184e7746b3a59135acac81cd2
Brianna Dirks <91711396+QueenOvHertz@users.noreply.github.com>
2022-04-19 21:16:23 -0400

    Add files via upload

--- src/myprogram.py
+++ src/myprogram.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python
 import os
-import string
 import random
+import string
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
 
 class MyModel:
-    """
-    This is a starter model to get you started. Feel free to modify this file.
-    """
 
     @classmethod
     def load_training_data(cls):
-        # your code here
-        # this particular model doesn't train
-        return []
+        #Update path according to machine
+        path = "C:\\Users\\Bri\\Documents\\Bri Stuff\\ECU\\2022 Spring\\Natural Language Processing\\cse447-project-main\\cse447-project-main\\example\\"
+        file_name = os.listdir(path)[0]
+        train_file = os.path.join(path, file_name)
+        return train_file
+
 
     @classmethod
+    #No change
     def load_test_data(cls, fname):
-        # your code here
         data = []
         with open(fname) as f:
             for line in f:
@@ -27,17 +27,42 @@ class MyModel:
         return data
 
     @classmethod
+    #No change
     def write_pred(cls, preds, fname):
         with open(fname, 'wt') as f:
             for p in preds:
                 f.write('{}\n'.format(p))
 
     def run_train(self, data, work_dir):
-        # your code here
-        pass
+        #enter data by line
+        textToPredict = []
+        with open(data, 'r') as f:
+            textToPredict = f.readlines()
+        #break lines into words
+        chars = {}
+        char_num = 0
+        for word in textToPredict:
+            #break words into characters
+            for char in word:
+                char_num += 1
+                #if character is not followed by a newline or space, 
+                #add space for new character
+                #unless the line is blank
+                if char != '\n' and char != ' ':
+                    try:
+                        chars[char] = chars[char] + 1
+                    except:
+                        chars[char] = 1
+        temp = []
+        for char in chars.keys():
+            if chars[char] < char_num * 0.00002:
+                temp.append(char)
+        for char in temp:
+            chars.pop(char)
+        sorted_chars = reversed(sorted(chars.items(), key=lambda kv: kv[1]))
+        self.train_data = sorted_chars
 
     def run_pred(self, data):
-        # your code here
         preds = []
         all_chars = string.ascii_letters
         for inp in data:
@@ -47,29 +72,42 @@ class MyModel:
         return preds
 
     def save(self, work_dir):
-        # your code here
-        # this particular model has nothing to save, but for demonstration purposes we will save a blank file
+        #records the list of characters from self.train_data
+        #obtained in run_train to model.checkpoint.txt
         with open(os.path.join(work_dir, 'model.checkpoint'), 'wt') as f:
-            f.write('dummy save')
+            for char in self.train_data:
+                f.write('{}: {}\n'.format(char[0], char[1]))
 
     @classmethod
     def load(cls, work_dir):
-        # your code here
-        # this particular model has nothing to load, but for demonstration purposes we will load a blank file
+        #loads the saved list of characters back into the model for
+        #predictions when testing 
+        char_list = []
         with open(os.path.join(work_dir, 'model.checkpoint')) as f:
-            dummy_save = f.read()
-        return MyModel()
+            char_list = f.readlines()
+        chars = []
+        for record in char_list:
+            char, char_num = record.split(': ')
+            chars.append((char, int(char_num[:-1])))
+        model = MyModel()
+        model.train_data = chars
+        #MyModel() is now updated and ready to predict
+        return model
 
 
 if __name__ == '__main__':
+    #Update paths depending on machine
     parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
     parser.add_argument('mode', choices=('train', 'test'), help='what to run')
     parser.add_argument('--work_dir', help='where to save', default='work')
-    parser.add_argument('--test_data', help='path to test data', default='example/input.txt')
-    parser.add_argument('--test_output', help='path to write test predictions', default='pred.txt')
-    args = parser.parse_args()
+    parser.add_argument('--test_data', help='path to test data', default='C:\\Users\\Bri\\Documents\\Bri Stuff\\ECU\\2022 Spring\\Natural Language Processing\\cse447-project-main\\cse447-project-main\\example\\input.txt')
+    parser.add_argument('--test_output', help='path to write test predictions', default='C:\\Users\\Bri\\Documents\\Bri Stuff\\ECU\\2022 Spring\\Natural Language Processing\\cse447-project-main\\cse447-project-main\\example\\pred.txt')
+    #Specify whether to 'train' or 'test' the Model
+    args = parser.parse_args(['train'])
+    
 
     random.seed(0)
+
 
     if args.mode == 'train':
         if not os.path.isdir(args.work_dir):
